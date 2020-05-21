21e1f4140e6c8c6a75607b04b0b6c61a97fe5631
tarunlalchandani <tarunlalchandani4236562@gmail.com>
2020-05-21 16:21:51 +0530

    Jobs Ready

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+#Create A Blog models
+#title
+#pub_date
+#body
+#image
+
+#Add the Blog app to the local_settings
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
