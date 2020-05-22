dac806be1112806d49325682e861a7aff6807821
tarunlalchandani <tarunlalchandani4236562@gmail.com>
2020-05-22 18:22:27 +0530

    2nd Project completed

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length = 255)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
 
 #Create A Blog models
 #title
