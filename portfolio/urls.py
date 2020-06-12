3fbabd1e6f96b91cc5ddc9e12b7200a9574bfc21
tarunlalchandani <tarunlalchandani4236562@gmail.com>
2020-06-12 14:46:36 +0530

    portfolio and ggrade stuff

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,6 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
     path('',jobs.views.home,name='home'),
-    path('blog/',include('blog.urls')),
-] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
